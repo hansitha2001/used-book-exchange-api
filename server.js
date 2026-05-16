d43105aa52fd216f59cc220ee716f90847fc0fd9
Hansitha Marasinghe <hansithamarasinghe@gmail.com>
2026-05-16 23:18:30 +0530

    Created frontend

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// Rout
 app.use('/api/books', require('./routes/books'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/orders', require('./routes/orders'));
